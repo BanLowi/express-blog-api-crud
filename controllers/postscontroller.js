8e9e9677ffc4c01d549b2e4961720c990e0d427d
BanLowi <lowi40k@gmail.com>
2025-11-27 00:40:50 +0100

    show request works

---
+++ controllers/postscontroller.js
@@ -0,0 +1,46 @@
+const posts = require("../data/posts");
+
+function index(req, res) {
+
+    let filteredPosts = posts;
+
+    if (req.query.title) {
+        filteredPosts = posts.filter(post => pizza.ingredients.includes(req.query.title))
+    };
+
+    res.json(posts)
+}
+
+function show(req, res) {
+    const id = parseInt(req.params.id)
+
+    const findPost = posts.find(post => post.id == id);
+
+    if (!findPost) {
+
+        return res.status(404).json({
+            error: "Not Found",
+            message: "Post not found"
+        })
+    }
+
+    res.json(findPost)
+}
+
+function store(req, res) {
+    res.send("Store a new post here")
+}
+
+function update(req, res) {
+    res.send(`Update the post with this id: ${req.params.id}`)
+}
+
+function modify(req, res) {
+    res.send(`Modify a part of the post with this id: ${req.params.id}`)
+}
+
+function destroy(req, res) {
+    res.send(`Delete the post with this id: ${req.params.id}`)
+}
+
+module.exports = { index, show, store, update, modify, destroy }
--- routers/posts.js
+++ routers/posts.js
@@ -1,43 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const posts = require("../posts");
+const postController = require("../controllers/postscontroller")
 
 /* Index */
-router.get("/", (req, res) => {
-
-    res.json(posts)
-})
+router.get("/", postController.index)
 
 /* Show */
-router.get("/:id", (req, res) => {
-    const findPost = posts.find(post => post.id == req.params.id)
-    console.log(findPost);
-
-    res.json(findPost)
-})
+router.get("/:id", postController.show)
 
 /* Store */
-router.post("/", (req, res) => {
-
-    res.send("Store a new post here")
-})
+router.post("/", postController.store)
 
 /* Update */
-router.put("/:id", (req, res) => {
-
-    res.send(`Update the post with this id: ${req.params.id}`)
-})
+router.put("/:id", postController.update)
 
 /* Modify */
-router.patch("/:id", (req, res) => {
-
-    res.send(`Modify a part of the post with this id: ${req.params.id}`)
-})
+router.patch("/:id", postController.modify)
 
 /* Destroy */
-router.delete("/:id", (req, res) => {
-
-    res.send(`Delete the post with this id: ${req.params.id}`)
-})
+router.delete("/:id", postController.destroy)
 
 module.exports = router
